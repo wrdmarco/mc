c15114410c4172af86d2c1f481125c0365f059cd
wrdmarco <marco@madeliefweide.nl>
2024-10-16 15:06:00 +0200

    Update unicorn.lua

--- unicorn.lua
+++ unicorn.lua
@@ -1221,17 +1221,19 @@ local image, palette = {
     {0.623529, 0.349020, 0.117647},
 }
 
-term.clear()
-for i = 0, #palette do term.setPaletteColor(2^i, table.unpack(palette[i])) end
+mon=peripheral.wrap("left")
+
+mon.clear()
+for i = 0, #palette do mon.setPaletteColor(2^i, table.unpack(palette[i])) end
 for y, r in ipairs(image) do
-    term.setCursorPos(1, y)
-    term.blit(table.unpack(r))
+    mon.setCursorPos(1, y)
+    mon.blit(table.unpack(r))
 end
 end
 sleep(0.04)
 read()
-for i = 0, 15 do term.setPaletteColor(2^i, term.nativePaletteColor(2^i)) end
-term.setBackgroundColor(colors.black)
-term.setTextColor(colors.white)
-term.setCursorPos(1, 1)
-term.clear()
+for i = 0, 15 do mon.setPaletteColor(2^i, mon.nativePaletteColor(2^i)) end
+mon.setBackgroundColor(colors.black)
+mon.setTextColor(colors.white)
+mon.setCursorPos(1, 1)
+mon.clear()
